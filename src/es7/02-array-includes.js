7dd2a8da6014b21c3e761ee20b3d6ea2dfb0ae4b
Karla Sequen <karlasequen@gmail.com>
2022-10-03 21:46:38 -0600

    add es7

---
+++ src/es7/02-array-includes.js
@@ -0,0 +1,12 @@
+let numbers = [1, 3, 4, 6, 7, 8];
+
+console.log(numbers.includes(4));
+
+console.log(numbers.includes(10));
+
+
+const names = ['oscar', 'david', 'ana'];
+
+console.log(names.includes('oscar'));
+console.log(names.includes('pedro'));
+console.log(names.includes('Oscar'));
