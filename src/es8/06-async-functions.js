5a3ab49bf329023ff77d1546f4098e8a8a20f0a0
Karla Sequen <karlasequen@gmail.com>
2022-10-03 22:07:19 -0600

    es8 async functions

---
+++ src/es8/06-async-functions.js
@@ -0,0 +1,23 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Async!!'), 2000)
+            : reject(new Error('Error!!!'));
+    })
+}
+
+const anotherFn = async () => {
+    const something = await fnAsync();
+    console.log(something);
+    console.log('Hi');
+}
+
+console.log('Before');
+anotherFn();
+console.log('After');
+
+
+////////
+console.log('Before2');
+await anotherFn();
+console.log('After2');
